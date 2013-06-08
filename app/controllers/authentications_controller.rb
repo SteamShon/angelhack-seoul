ade8c3cc3850b419d63616768e81a5e6a01ec61b
SteamShon <shom83@gmail.com>
2013-06-08 18:59:52 +0900

    .

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -36,6 +36,7 @@ class AuthenticationsController < ApplicationController
   # POST /authentications.json
   def create
     auth = request.env["omniauth.auth"]
+    current_user
     current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
     flash[:notice] = "Authentication successful."
     redirect_to authentications_url
